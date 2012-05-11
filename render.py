9a3fb5a0ccd37ded657c512ac220beccfd69e75d
ajones <ajones@weogeo.com>
2012-05-11 11:23:35 -0700

    Split up option documentation into groups for easier understanding.

--- render.py
+++ render.py
@@ -4,81 +4,16 @@ import tilesettings, tilerenderer, tools, bound
 
 from processors import Renderer
 
-from optparse import OptionParser
+from optparse import OptionParser, OptionGroup
 
 usage = "usage: %prog [options] <style_file.xml>"
 parser = OptionParser(usage=usage)
-
-# map extent options
-parser.add_option("-n", "--north", dest="north", default=None, type="float",
-                  help="Northern map extent default: [projection max]")
-parser.add_option("-e", "--east", dest="east", default=None, type="float",
-                  help="Eastern map extent default: [projection max]")
-parser.add_option("-s", "--south", dest="south", default=None, type="float",
-                  help="Southern map extent default: [projection max]")
-parser.add_option("-w", "--west", dest="west", default=None, type="float",
-                  help="Western map extent default: [projection max]")
-
-parser.add_option("-p", "--projection", dest="projection", default="geo", type="string",
-                  help="The projection to be used. options [geo, sm], default: geo")
 
 # pipeline selection options
 parser.add_option("-T", "--tile-type", dest="tile_type", default="xyz", type="string",
                   help="Type of tiles to generate. options: [xyz], default: xyz")
 parser.add_option("-R", "--tile-renderer", dest="tile_renderer", default="mapnik", type="string",
                   help="Renderer to use in producing tiles. options [mapnik], default: mapnik")
-
-# watermarking options
-parser.add_option("--watermark", dest="watermark", default=False, action="store_true",
-                  help="Enable watermarking")
-parser.add_option("--watermark_file", dest="watermark_file", default="./watermark.png", type="string",
-                  help="The file to use for watermarking. Default is ./watermark.png")
-
-# file handling options
-parser.add_option("-k", "--keep-existing", dest="overwrite", default=True, action="store_false",
-                  help="Keep files that already exist? default: not kept")
-parser.add_option("-f", "--output-format", dest="output_format", default='png', type="string",
-                  help="The output file format. Only 'png' and 'jpeg' are allowed default: png")
-parser.add_option("-b", "--base-directory", dest="base_dir", default='./tiles/', type="string",
-                  help="The base directory in which to store tiles default: ./tiles/")
-parser.add_option('-g', '--log-file', dest='log_file', default='./render.log', type='string',
-                  help="The file to append long information to, default: 'render.log' in the current directory")
-parser.add_option('-i', '--pixel-bits', dest="pixel_bits", default=8, type='int',
-                  help="The bits per pixel to use for .png renderings, allowed are '8', '24', and '32'. default: 8")
-parser.add_option("-d", "--do-not-delete-metapanels", dest="preserve_metapanels", default=False, action="store_true",
-                  help="Preserve meta panels after finished cutting tiles from them (default: no)")
-parser.add_option("-W", "--website-mode", dest="website_mode", default=False, action="store_true",
-                  help="Produce special output designed for control by/interaction wtih a website. default: no")
-parser.add_option('-H', '--add-highlight', dest='highlight', default=None, type='string',
-                  help="Generate a json highlight layer for the bounds. Argument is the file location to store the json. Default: do not generate")
-
-# s3 upload options
-parser.add_option("--s3-file", dest="s3_file", default="/data/.s3",
-                  help="location of the file containing s3 keys. Only needed when s3-access and s3-secret are not provided directly. Default is /data/.s3")
-parser.add_option("--s3-access", dest="s3_access", default=None,
-                  help="S3 access key. Default is to gather it from the s3 file")
-parser.add_option("--s3-secret", dest="s3_secret", default=None,
-                  help="S3 secret key. Default is to gather it from the s3 file")
-parser.add_option("--s3-bucket", dest="s3_bucket", default=None,
-                  help="Bucket to use when uploading to s3. S3 uploads will not be performed unless this option is set")
-parser.add_option("--s3-prefix", dest="s3_prefix", default=None,
-                  help="prefix added to the tile location. S3 uploads will not be performed unless this option is set")
-
-# kml/preview image/tilepack related options
-parser.add_option("-P", "--generate-preview-images", dest="do_previews", default=False, action="store_true",
-                  help="Generate a 316x316 preview along with the tiles, and resample a 61x61 thumbnail from that. default: do not generate")
-parser.add_option("-K", "--generate-kml-preview", dest="do_kml", default=False, action="store_true",
-                  help="Generate a kml preview image from the bounds. These always have one max dimension of 1024, and are always geographic/png. default: do not generate")
-parser.add_option("-Z", "--generate-tilepack", dest="generate_tilepack", default=None, type="string",
-                  help="Generate a tilepack (with the given name) from the rendered tiles/preview image/kml. Turns on the -P and -K flags automatically. default: do not generate")
-
-# zoom level options
-parser.add_option("-c", "--scale", dest="scale", default=0, type="int",
-                  help="The scale of the furthest zoomed out zoom level")
-parser.add_option("-l", "--first-scale", dest="first_scale", default=1, type="int",
-                  help="The first scale to start rendering at, default: the input to '-c'")
-parser.add_option("-z", "--zoom-levels", dest="zoom_levels", default=5, type="int",
-                  help="The number of zoom levels to render default: 5")
 
 # kamap specific options
 parser.add_option("-m", "--meta-size", dest="meta_size", default=20, type="int",
@@ -87,6 +22,86 @@ parser.add_option("-m", "--meta-size", dest="meta_size", default=20, type="int",
 # console output options
 parser.add_option("-v", "--verbose", dest="verbose", default=False, action="store_true",
                   help="more detailed output")
+
+
+# map extent options
+map_group = OptionGroup(parser, "Geographic Options")
+map_group.add_option("-n", "--north", dest="north", default=None, type="float",
+                     help="Northern map extent default: [projection max]")
+map_group.add_option("-e", "--east", dest="east", default=None, type="float",
+                     help="Eastern map extent default: [projection max]")
+map_group.add_option("-s", "--south", dest="south", default=None, type="float",
+                     help="Southern map extent default: [projection max]")
+map_group.add_option("-w", "--west", dest="west", default=None, type="float",
+                     help="Western map extent default: [projection max]")
+
+map_group.add_option("-p", "--projection", dest="projection", default="geo", type="string",
+                     help="The projection to be used. Note: source data files must match this projection at this time. options [geo, sm], default: geo")
+
+# zoom level options
+map_group.add_option("-c", "--scale", dest="scale", default=0, type="int",
+                     help="The scale of the furthest zoomed out zoom level")
+map_group.add_option("-l", "--first-scale", dest="first_scale", default=1, type="int",
+                     help="The first scale to start rendering at, default: the input to '-c'")
+map_group.add_option("-z", "--zoom-levels", dest="zoom_levels", default=5, type="int",
+                     help="The number of zoom levels to render default: 5")
+
+
+parser.add_option_group(map_group)
+
+
+watermark_group = OptionGroup(parser, "Watermarking")
+# watermarking options
+watermark_group.add_option("--watermark", dest="watermark", default=False, action="store_true",
+                           help="Enable watermarking")
+watermark_group.add_option("--watermark_file", dest="watermark_file", default="./watermark.png", type="string",
+                           help="The file to use for watermarking. Default is ./watermark.png")
+
+parser.add_option_group(watermark_group)
+
+# file handling options
+file_group = OptionGroup(parser, "File Settings")
+file_group.add_option("-k", "--keep-existing", dest="overwrite", default=True, action="store_false",
+                      help="Keep files that already exist? default: not kept")
+file_group.add_option("-f", "--output-format", dest="output_format", default='png', type="string",
+                      help="The output file format. Only 'png' and 'jpeg' are allowed default: png")
+file_group.add_option("-b", "--base-directory", dest="base_dir", default='./tiles/', type="string",
+                      help="The base directory in which to store tiles default: ./tiles/")
+file_group.add_option('-g', '--log-file', dest='log_file', default='./render.log', type='string',
+                      help="The file to append long information to, default: 'render.log' in the current directory")
+file_group.add_option('-i', '--pixel-bits', dest="pixel_bits", default=8, type='int',
+                      help="The bits per pixel to use for .png renderings, allowed are '8', '24', and '32'. default: 8")
+file_group.add_option("-d", "--do-not-delete-metapanels", dest="preserve_metapanels", default=False, action="store_true",
+                      help="Preserve meta panels after finished cutting tiles from them (default: no)")
+file_group.add_option("-W", "--website-mode", dest="website_mode", default=False, action="store_true",
+                      help="Produce special output designed for control by/interaction wtih a website. default: no")
+file_group.add_option('-H', '--add-highlight', dest='highlight', default=None, type='string',
+                      help="Generate a json highlight layer for the bounds. Argument is the file location to store the json. Default: do not generate")
+parser.add_option_group(file_group)
+
+# s3 upload options
+s3_group = OptionGroup(parser, "S3 Upload")
+s3_group.add_option("--s3-file", dest="s3_file", default="/data/.s3",
+                    help="location of the file containing s3 keys. Only needed when s3-access and s3-secret are not provided directly. Default is /data/.s3")
+s3_group.add_option("--s3-access", dest="s3_access", default=None,
+                    help="S3 access key. Default is to gather it from the s3 file")
+s3_group.add_option("--s3-secret", dest="s3_secret", default=None,
+                    help="S3 secret key. Default is to gather it from the s3 file")
+s3_group.add_option("--s3-bucket", dest="s3_bucket", default=None,
+                    help="Bucket to use when uploading to s3. S3 uploads will not be performed unless this option is set")
+s3_group.add_option("--s3-prefix", dest="s3_prefix", default=None,
+                    help="prefix added to the tile location. S3 uploads will not be performed unless this option is set")
+parser.add_option_group(s3_group)
+
+# kml/preview image/tilepack related options
+tilepack_group = OptionGroup(parser, "WeoGeo Tilepack Settings")
+tilepack_group.add_option("-P", "--generate-preview-images", dest="do_previews", default=False, action="store_true",
+                          help="Generate a 316x316 preview along with the tiles, and resample a 61x61 thumbnail from that. default: do not generate")
+tilepack_group.add_option("-K", "--generate-kml-preview", dest="do_kml", default=False, action="store_true",
+                          help="Generate a kml preview image from the bounds. These always have one max dimension of 1024, and are always geographic/png. default: do not generate")
+tilepack_group.add_option("-Z", "--generate-tilepack", dest="generate_tilepack", default=None, type="string",
+                          help="Generate a tilepack (with the given name) from the rendered tiles/preview image/kml. Turns on the -P and -K flags automatically. default: do not generate")
+parser.add_option_group(tilepack_group)
 
 if __name__ == '__main__':
     (opts, args) = parser.parse_args()
