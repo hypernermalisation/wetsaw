bcfc57b19e376a5e3e955fe41f1919f8b4487109
ajones <ajones@weogeo.com>
2012-05-11 11:04:48 -0700

    Update the setup.py script.

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 from distutils.core import setup
-import os
-
-svnversion=""
-svnversion=os.popen(''' svn info | sed -e '/^URL:/!d' -e 's/^.*\/tags\///g' -e 's/\/.*$$//g' -e '/^URL:/d' -e 's/^[A-Z,a-z]*-//g' -e 's/_/\./g' ''',"r").readline()
-if svnversion == "":
-  svnversion = os.popen(''' svn info | sed -e '/^Revision:/!d' -e 's/.* /svn/g' ''', "r").readline()
-
-if svnversion == "":
-  svnversion = "svn"
 
 setup(name='wetsaw',
-      version=svnversion.rstrip(),
-      description='Tile Renderer',
+      version="1.0.0",
+      description='A Mapnik-based XYZ tile rendering tool.',
       author='WeoGeo - Adam Jones',
       author_email='ajones@weogeo.com',
-      url='http://godzilla.weogeo.com/svn/wetsaw/',
+      url='http://github.com/WeoGeo/wetsaw/',
       packages=['processors'],
       py_modules = ['bound', 'import', 'make_baseimage', 'name', 'parser', 'render', 's3push', 'singleimage', 'tilecutter', 'tilecutter', 'tilerenderer', 'tilesettings', 'tools' ],
       scripts = [ 'scripts/render.py' ],
